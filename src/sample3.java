0f3bff2dea59592cf47a69fc0bc6e5c3c36cd5d5
g16934os <g16934os@m20206.tsuda.ac.jp>
2018-04-16 13:40:23 +0900

    flutdl

---
+++ src/sample3.java
@@ -0,0 +1,8 @@
+
+public class sample3 {
+	public static void main(String args[]){
+		System.out.println("プログラミングなんて嫌い");
+		System.out.println("でもやらないと卒業できない");
+		System.out.println("頑張ります...。");
+	}
+}
